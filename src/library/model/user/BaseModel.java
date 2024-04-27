d3ab642b5a5a5678ff823d8fdfdfd03af933276d
bobur <davlatboyev2024@gmail.com>
2024-04-27 02:34:07 -0700

    go on

--- src/library/model/user/BaseModel.java
+++ src/library/model/user/BaseModel.java
@@ -1,4 +1,11 @@
 package library.model.user;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class BaseModel {
+
+    {id = UUID.randomUUID();}
+    protected UUID id;
+    protected LocalDate localDate;
 }
